8301240091fc6d0f42c7c9d295ad9288bfdc68ff
annieouseph8 <annieouseph872@gmail.com>
2026-03-19 10:46:28 +0530

    Added sample code for testing

--- sample.py
+++ sample.py
@@ -1,5 +1,20 @@
-secret = "12334"
+import sqlite3
+API_KEY = "sk_live_51MhZ2kL90pQ2zX7rN" 
 
-for i in range (5):
-    print(i)
-print("Done")
+def get_user_data(user_id, user_list=[]): 
+    
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    
+    query = f"SELECT * FROM users WHERE id = {user_id}" 
+    cursor.execute(query)
+    
+    data = cursor.fetchone()
+    user_list.append(data)
+    
+    if len(user_list) > 0:
+        for i in range(len(user_list)):
+            if user_list[i] is not None:
+                return user_list[i]
+    
+    return None
